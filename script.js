f843e9ba34b3a4cd117fea54d78e47a77cfeadca
einn9 <kratosmana@gmail.com>
2025-04-26 07:42:13 -0300

    Update HTML and add JavaScript operate function

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-const add = function(a, b) {
+function addition(a, b) {
 	return a + b;
 };
 
-const subtract = function(a, b) {
+function subtraction(a, b) {
 	return a - b;
 };
 
-const multiply = function(array) {
+function multiplication(array) {
     return array.reduce((sum, total) => sum * total);
   };
 
-const divide = function(array) {
+function division(array) {
     return array.reduce((sum, total) => sum / total);
 };
+
+const numberOne = 4;
+const numberTwo = 2;
+console.log(addition(numberOne, numberTwo));
+console.log(subtraction(numberOne, numberTwo));
+console.log(multiplication([numberOne, numberTwo]));
+console.log(division([numberOne, numberTwo]));
+
+
+function operator() {
+    let userInput = prompt("which operator do you want to use?")
+
+    if (userInput === "addition") {
+        return addition(numberOne, numberTwo);
+    }
+    if (userInput === "subtraction") {
+        return subtraction(numberOne, numberTwo);
+    }
+    if (userInput === "multiplication") {
+        return multiplication([numberOne, numberTwo]);
+    }
+    if (userInput === "division") {
+        return division([numberOne, numberTwo]);
+    }
+    else {
+        return "ERROR";
+    }
+}
+
+console.log(operator());
